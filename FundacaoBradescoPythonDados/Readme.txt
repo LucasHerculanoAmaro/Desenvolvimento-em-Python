e4d39664dd4795a401ed197981d148bcd7e7a324
Lucas Amaro <lucash.96@hotmail.com>
2024-03-12 18:11:47 -0300

    Preparando o ambiente virtual e instalando o Django em diferentes pastas com o ambiente virtual

---
+++ FundacaoBradescoPythonDados/Readme.txt
@@ -0,0 +1,3 @@
+Atualização de gits
+
+12/02/2024 - Criando projetos e instalando Django em pastas diferentes, utilizando Ambiente Virtual.
---
+++ FundacaoBradescoPythonDados/cliente-1/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\lucas\Documents\Exercícios Python\Desenvolvimento-em-Python\FundacaoBradescoPythonDados\cliente-1\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
